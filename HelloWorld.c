470c3f527d948c9782d23d8609a01449a107a6f8
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-21 16:07:24 +0300

    initialize c learning project

---
+++ HelloWorld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // printf("\"I like poppys\"  - One grate man");
+
+    int x;
+    x = 1234;
+
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'C';
+
+    return 0;
+}
+
+/*
+
+
+
+*/
