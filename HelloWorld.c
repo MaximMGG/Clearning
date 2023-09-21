47115e6c9ee53cd31601f916e4ce5b6500af8fa5
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-09-21 16:16:28 +0300

    variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -9,8 +9,13 @@ int main() {
     x = 1234;
 
     int age = 21;
-    float gpa = 2.05;
-    char grade = 'C';
+    float gpa = 2.05983001;
+    char grade = 'C';//ingle character
+    char name[] = "Boby";// array of characters (String)
+
+    printf("You name is %s and you are %d years old\n", name, age); // %s - array of characters, %d - int
+    printf("Your avarage is %c\n", grade); // %c - single character
+    printf("Your gpa is %.2f", gpa);// %f - float (if %.2f) count of number ofter point.
 
     return 0;
 }
